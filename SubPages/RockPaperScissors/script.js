5f4a6f458ee916baa0b35e5f1e8886f2467f110b
Noxein <138109221+Noxein@users.noreply.github.com>
2023-07-10 16:30:40 +0200

    Final version of RockPaperScissors, 5 rounds against computer + if statment to determin the victor

--- SubPages/RockPaperScissors/script.js
+++ SubPages/RockPaperScissors/script.js
@@ -1,5 +1,5 @@
 //Generates Paper Rock or Scissors
-function randomRPS(){
+function getComputerChoice(){
     randomNum = (Math.random()*3)+1;
     if(randomNum>1&&randomNum<2){
         randomNum = "rock"
@@ -8,43 +8,93 @@ function randomRPS(){
     }else{
         randomNum = "scissors"
     }
+    console.log("Machine chosed "+randomNum);
     return randomNum;
-}
 
-console.log(randomRPS());
+}
 
 // User input
-let userInput = "scissors";
-
-//Checking if user input is paper rock or scissors and puts it to lower case
-function checkLetters(){
-    lowCas = userInput.toLowerCase();
-    if(lowCas=="rock"||lowCas=="paper"||lowCas=="scissors"){
-        return lowCas;
+//correctInput(userInput);
+function correctInput(){
+    let userInput = prompt("Chose Paper Rock or Scissors")
+    lowCase = (userInput.toLowerCase());
+    if(lowCase=="rock"||lowCase=="paper"||lowCase=="scissors"){
+        console.log("You chosed " + lowCase);
+        return lowCase;
     }else{
-    console.log("Enter Rock Paper or Scissors");
+        again = prompt("Wrong input, try again. Chose Paper Rock or Scissors");
+        correctInput(again);
     }
 }
 
-console.log(checkLetters());
+let Machine = 0;
+let Player = 0;
+let Draw = 0
+let Time = "";
 
-//Final function to check who wins or if its draw
-function RPS(){
-    if(randomNum===lowCas){
-        console.log("Its a draw!");
-
-    }else if((randomNum=="rock") && (lowCas=="scissors")){
-        console.log("You Lose!");
-
-    }else if((randomNum=="paper") && (lowCas=="rock")){
-        console.log("You Lose!");
+function grammar(Draw){
+    if(Draw<=1){
+        return "Draw"
+    }else{
+        return "Draws"
     }
+}
 
-    else if((randomNum=="scissors") && (lowCas=="paper")){
-        console.log("You Lose!");
+function grammarT(Time){
+    if(Time<=1){
+        return "Time"
+    }else{
+        return "Times"
+    }
+}
+function winner(){
+    if((Machine===Player)||(Draw===5)){
+        alert("Out of 5 rounds its a Draw! There were " + Draw + " " + grammar(Draw))
+    }
+    else if(Player>Machine){
+        alert("You won! "+Player + " out of 5, there were " + Draw + " " + grammar(Draw))
+    }
+    else if(Machine>Player){
+        alert("You lose! Coputer won "+Machine + " out of 5, there were " + Draw + " " + grammar(Draw) +", you won " + Player + " " + grammarT(Time))
     }
     else{
-        console.log("You Win!");
+        alert("Error");
     }
 }
-    console.log(RPS())
+
+function oneRound(playerSelection, computerSelection){
+    randomNum;
+    if(computerSelection===playerSelection){
+        alert("Its a draw");
+        return Draw++;
+    }else if ((computerSelection=="paper")&&(playerSelection=="rock")){
+        alert("You lose Paper beats Rock");
+        return Machine++;
+
+    }else if((computerSelection=="scissors")&&(playerSelection=="paper")){
+        alert("You lose Scissors beats Paper");
+        return Machine++;
+
+    }else if((computerSelection=="rock")&&(playerSelection=="scissors")){
+        alert("You lose Rock beats Scissors");
+        return Machine++;
+
+    }else{
+        alert("You win");
+        return Player ++;
+    }
+}
+
+
+function game(){
+    for(let i=0; i<5 ;i++){
+        oneRound(correctInput(),getComputerChoice());
+        console.log(Machine);
+        console.log(Player);
+        console.log(Draw);
+
+        }
+    }
+
+game();
+winner();
